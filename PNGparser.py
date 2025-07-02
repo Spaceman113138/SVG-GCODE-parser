5a14a344478c2cd9b0c1c80af10992b8cb138c3c
= <ianspace3138@gmail.com>
2025-07-01 23:48:58 -0500

    working 16 bit

--- PNGparser.py
+++ PNGparser.py
@@ -48,77 +48,84 @@ def readPNG(path: str):
     plt.show()
 
 
+def parseBytesToInts(line:bytes, depth: int) -> list[int]:
+    values: list[int] = []
 
+    match depth:
+        case 8:
+            for byte in line:
+                values.append(int(byte))
+        case 16:
+            for i in range(int(len(line) / 2)):
+                thing = struct.unpack(">H", line[i*2:(i+1)*2])
+                values.append(int(thing[0]))
+        case _:
+            raise Exception("Not implemented or valid")
+
+    return values 
 
 
 def parseData(data: bytes, channels, bytesPerLine: int, depth, colortype):
-    reconstructedLines = reconstructData(data, int(channels), int(depth), int(bytesPerLine))
-    combinedData: bytearray = bytearray.fromhex("")
+    reconstructedLines: list[bytes] = reconstructData(data, int(channels), int(depth), int(bytesPerLine))
+
+    combinedData: list[int] = []
+
     for line in reconstructedLines:
-        combinedData += line # type: ignore
+        combinedData.extend(parseBytesToInts(line, depth))
     
-    depth = depth / 8
+    depthByte = int(depth / 8)
 
     pixleData = []
     match channels:
         case 4:
-            formatString = "BBBB" if depth == 8 else "HHHH"
+            i = 0
             while len(combinedData) > 0:
-                r,g,b,a = struct.unpack(formatString, combinedData[0:4 * depth])
-                combinedData = combinedData = combinedData[4 * depth:]
+                r,g,b,a = combinedData[i * 4: (i+1) * 4]
+                combinedData = combinedData[(i+1) * 4:]
+                r = int(r / (2 ** depth - 1) * 255)
+                g = int(g / (2 ** depth - 1) * 255)
+                b = int(b / (2 ** depth - 1) * 255)
+                a = int(a / (2 ** depth - 1) * 255)
+
                 pixleData.append([r,g,b,a])
-        case 3:
-            formatString = "BBB" if depth == 8 else "HHH"
-            while len(combinedData) > 0:
-                r,g,b = struct.unpack(formatString, combinedData[0:3 * depth])
-                combinedData = combinedData[3 * depth:]
-                pixleData.append([r,g,b,255])
-        case 2:
-            formatString = "BB" if depth == 8 else "HH"
-            while len(combinedData) > 0:
-                w,a = struct.unpack(formatString, combinedData[0:2 * depth])
-                combinedData = combinedData[2 * depth:]
-                pixleData.append([w,w,w,a])
         case _:
             raise Exception("I should really implement this")
         
     return pixleData
 
-    
-
 
 def reconstructData(data, channels: int, depth, bytesPerLine:int):
-    if depth < 8:
-        raise Exception("CRY cuz bits hard to deal with :(")
-    if depth > 8:
-        raise Exception("Cry less cuz easier :)")
     
     depthOffset: int = int(depth / 8) #use to control offset because depth 16 uses 2 bytes
-    step = channels * depthOffset
+    bpp = math.ceil(channels * depthOffset)
 
     #split into lines to make easier to deal with
-    lines: list[bytearray] = []
+    values:list[bytes] = []
     while len(data) > 0:
-        lines.append(bytearray(data[0:int(bytesPerLine)]))
-        data = data[int(bytesPerLine):]
+        line = data[0: bytesPerLine]
+        data = data[bytesPerLine:]
+        values.append(line)
+        #values.append(parseBytesToInts(line, depth))
 
-    reconstructedLines: list[bytearray] = []
+    reconstructedLines: list[bytes] = []
 
-    for line in lines:
-        filter = line.pop(0)
-        newLine: bytearray = bytearray.fromhex("")
+    for line in values:
+        filter = line[0]
+        line = line[1:]
+        newLine: bytes = b""
         match filter:
             case 0:
                 reconstructedLines.append(line)
             case 1:
-                i: int = 0
-                while i < len(line):
+                print(line.hex("/"))
+                for i in range(len(line)):
                     x = line[i]
-                    a = 0 if i < step else newLine[i - step]
-                    newLine.append((x + a) % 256)
-                    i += depthOffset
+                    a = 0 if i < bpp else newLine[i-bpp]
+                    newValue = (x + a) % 256
+                    newLine += newValue.to_bytes(1)
                 reconstructedLines.append(newLine)
             case 2:
+                raise Exception("All wrong")
                 i = 0
                 while i < len(line):
                     x = line[i]
@@ -127,6 +134,7 @@ def reconstructData(data, channels: int, depth, bytesPerLine:int):
                     i += depthOffset
                 reconstructedLines.append(newLine)
             case 3:
+                raise Exception("All wrong")
                 i = 0
                 while i < len(line):
                     x = line[i]
@@ -138,16 +146,19 @@ def reconstructData(data, channels: int, depth, bytesPerLine:int):
                 reconstructedLines.append(newLine)
             case 4:
                 i = 0
+                #print(line)
                 while i < len(line):
                     x = line[i]
-                    a = 0 if i < step else newLine[i-step]
+                    a = 0 if i < bpp else newLine[i-bpp]
                     b = 0 if len(reconstructedLines) == 0 else reconstructedLines[-1][i]
-                    c = 0 if len(reconstructedLines) == 0 or i < step else reconstructedLines[-1][i-step]
-                    newLine.append((x + PaethReconstruction(a,b,c)) % 256)
-                    i += depthOffset
+                    c = 0 if i < bpp or len(reconstructedLines) == 0 else reconstructedLines[-1][i-bpp]
+                    value = (x + PaethReconstruction(a, b, c)) % 256
+                    newLine += value.to_bytes(1)
+                    i += 1
                 reconstructedLines.append(newLine)
             case _:
                 raise Exception("Invalid Filter")
+
     return reconstructedLines
 
 
@@ -229,11 +240,4 @@ def parseIDHR(idhrChunk: dict):
 
 
 
-
-
-
-
-
-
-
-readPNG("testPNG\\basn4a08.png")
+readPNG("testPNG\\basn6a16.png")
